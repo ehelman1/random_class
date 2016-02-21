f034f83689e09c85b4f78dca834051da7a8dcd70
Edward Helman <eddhelman@gmail.com>
2016-02-21 12:03:31 -0500

    prints all pairs but left over

--- random_class.rb
+++ random_class.rb
@@ -1,16 +1,16 @@
 def class_pairing
 	array_of_class = %w[ed jon john jennifer dolly shane jonhnny aaron heather jenny marvin]
 
-		if array_of_class
+		while array_of_class.count >= 4
 			person_one = array_of_class.sample
 			array_of_class.delete(person_one)
 			person_two = array_of_class.sample
 			array_of_class.delete(person_two)
 			group = person_one + " " + "and" + " " + person_two
-
+			puts group
 
 		end
-	group
+	
 end	
 puts class_pairing
 
