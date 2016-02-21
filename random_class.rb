b5949c16419d5c42e9acf629040b8df01557d861
Edward Helman <eddhelman@gmail.com>
2016-02-21 11:56:25 -0500

    first pairing

--- random_class.rb
+++ random_class.rb
@@ -1,5 +1,16 @@
-array_of_class = %w[ed jon john jennifer dolly shane jonhnny aaron heather jenny marvin]
+def class_pairing
+	array_of_class = %w[ed jon john jennifer dolly shane jonhnny aaron heather jenny marvin]
 
-puts array_of_class.sample
+		if array_of_class
+			person_one = array_of_class.sample
+			array_of_class.delete(person_one)
+			person_two = array_of_class.sample
+			array_of_class.delete(person_two)
+			group = person_one + " " + "and" + " " + person_two
 
+
+		end
+	group
+end	
+puts class_pairing
 
