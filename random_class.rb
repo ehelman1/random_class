2821db3104c73e0f382bad3d7fbdbfebf09154ed
Edward Helman <eddhelman@gmail.com>
2016-02-21 12:39:48 -0500

    refactored 2

--- random_class.rb
+++ random_class.rb
@@ -1,6 +1,5 @@
-def class_pairing
-	array_of_class = %w[ed jon john jennifer dolly shane jonhnny aaron heather jenny marvin]
-
+def class_pairing(array_of_class)
+	
 		while array_of_class.count >= 4
 			person_one = array_of_class.sample
 			array_of_class.delete(person_one)
@@ -8,9 +7,8 @@ def class_pairing
 			array_of_class.delete(person_two)
 			group = person_one + " " + "and" + " " + person_two
 			puts group
-
 		end
-	array_of_class.join(" and ")
+	puts array_of_class.join(" and ")
 end	
-puts class_pairing
+class_pairing(["ed", "jon", "john", "jennifer", "dolly", "shane", "jonhnny", "aaron", "heather", "jenny", "marvin"])
 
